043960650d5842f8a55776067336cb9cb18cb3f8
Anderson fernandes <pires.fernandescg@yahoo.com.br>
2026-02-11 20:09:26 -0300

    adiciona código inicial do projeto

---
+++ script.js
@@ -0,0 +1,36 @@
+function realizarLogin(){
+    var email = document.getElementById("email").value 
+    var senha = document.getElementById("senha").value 
+
+if (!email) {
+    document.getElementById("email").style.borderColor = "tomato";
+    // document.getElementById("error_email").innerText = "O email e obrigatorio";
+    return;
+  }
+
+  if (!senha) {
+    document.getElementById("senha").style.borderColor = "tomato";
+    return;
+  }
+
+    if(email === "senai@gmail.com" && senha === "senai"){
+        window.location.href = "./home.html"
+    }
+    else{
+        alert("Credenciais incorretas")
+    }
+}
+
+function exibirSenha() {
+  var tipo_atual = document.getElementById("senha").type;
+
+  /* Condicao com IF ternario
+  document.getElementById(senha).type = tipo_atual === "text" ? "password" : "text"
+  */
+
+  if (tipo_atual === "text") {
+    document.getElementById("senha").type = "password";
+  } else if (tipo_atual === "password") {
+    document.getElementById("senha").type = "text";
+  }
+}
