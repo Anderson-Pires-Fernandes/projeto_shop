3d2179cea78402eb08ce3c02128c674e3c7629ee
Anderson fernandes <pires.fernandescg@yahoo.com.br>
2026-03-04 18:52:46 -0300

    adiciona função para carregar e exibir produtos da API

---
+++ pages/home/home.js
@@ -0,0 +1,32 @@
+async function carregarDados() {
+  var resposta = await fetch("http://localhost:3000/products");
+  var dados = await resposta.json();
+
+  dados.forEach((meuItem) => {
+    var meuProdutoHTML = `
+       <div class="produto">
+        <div class="produto_imagem_container">
+          <img
+            src=${meuItem.imagem}
+            class="produto_imagem"
+          />
+        </div>
+        <div class="produto_informacoes_container">
+          <h3>${meuItem.nome}</h3>
+          <span>${meuItem.raridade}</span>
+          <p>${meuItem.descricao}</p>
+          <span>R$ ${meuItem.preco}</span>
+          <button>Adicionar</button>
+        </div>
+      </div>
+    `;
+
+    document
+      .getElementById("lista")
+      .insertAdjacentHTML("beforeend", meuProdutoHTML);
+  });
+}
+carregarDados();
+
+
+// produtos.forEach(function (item) {});
